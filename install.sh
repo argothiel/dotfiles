d7ebc06ffdc0768c9d1a1618a4369bdbe10defff
argothiel <13932353+argothiel@users.noreply.github.com>
2025-01-12 13:57:54 +0100

    Detect distro

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd "$(dirname $0)"
+
+. /etc/os-release
+
+case $ID in
+  ubuntu) su -c ./install-ubuntu-root.sh
+    ;;
+
+  arch) echo "This is Arch Linux!"
+    ;;
+
+  centos) echo "This is CentOS!"
+    ;;
+
+  *) echo "This is an unknown distribution."
+      ;;
+esac
+
+su -c ./install-common-root.sh
