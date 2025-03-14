ada07416f8cdc72db05b009c970ec6da54e17e17
argothiel <argothiel@interia.pl>
2025-03-14 10:16:54 +0100

    Update neovim configuration
    Switch neovim to LazyVim. Install useful tooling.
    Configure clangd.
    Simplify neovim aliases.

---
+++ home/.config/nvim/lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.autoformat = false
+
+vim.g.clipboard = {
+  name = 'win32yank-wsl',
+  copy = {
+    ['+'] = 'win32yank -i --crlf',
+    ['*'] = 'win32yank -i --crlf',
+  },
+  paste = {
+    ['+'] = 'win32yank -o --lf',
+    ['*'] = 'win32yank -o --lf',
+  },
+  cache_enabled = true,
+}
+
