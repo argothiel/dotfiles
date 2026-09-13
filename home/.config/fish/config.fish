7036ff90e25de0e1db18329ec46ed696bdb29c7f
argothiel <13932353+argothiel@users.noreply.github.com>
2026-09-13 07:52:26 -0500

    Simplify config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -14,7 +14,7 @@ alias cd..='cd ..'
 alias getRoot='git rev-parse --show-toplevel 2>/dev/null || echo $DEFAULT_PROJECT_PATH'
 alias cdProject="cd $(getRoot) 2>/dev/null || cd"
 
-alias gitgr='date "+%a %b %d %T" && git log --graph --full-history --date=format-local:"%a %d %b %H:%M:%S" --max-count=35 --all --color --pretty=tformat:"%C(red)%h%C(reset) -%C(yellow)%d%C(reset) %s %C(green)(%cd) %C(bold blue)<%an>%C(reset)" --abbrev-commit'
+alias gitgr='date "+%a %b %d %T" && git log --graph --full-history --date=format-local:"%a %d %b %H:%M:%S" --max-count=33 --all --color --pretty=tformat:"%C(red)%h%C(reset) -%C(yellow)%d%C(reset) %s %C(green)(%cd) %C(bold blue)<%an>%C(reset)" --abbrev-commit'
 alias gitgrM='gitgr --max-count=10000'
 abbr nv nvim
 abbr nvp 'nvim -R -'
